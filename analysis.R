cc44895f1a527755a9fb2426625fde02e3a4f061
Vici13 <victoria@jorda.ch>
2024-09-13 12:29:22 +0200

    r

--- analysis.R
+++ analysis.R
@@ -17,3 +17,9 @@ pwr.r.test(n=250, r=0.1, sig.level = 0.05, power = NULL, alternative = c("two.si
 pwr.r.test(n=250, r=0.3, sig.level = 0.05, power = NULL, alternative = c("two.sided"))
 pwr.r.test(n=250, r=0.5, sig.level = 0.05, power = NULL, alternative = c("two.sided"))
 
+
+setwd("\\Users\\victo\\OneDrive - Universität Basel\\Dokumente\\gittest\\G02_Victoria")
+mydata <- read.table("mydata.txt", header=T, sep="\t")
+
+View(mydata)
+summary(mydata)
